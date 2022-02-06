91365c3f3f91ee9479f0a93216251669528ac7db
Michael <Monkeycase14@gmail.com>
2022-02-06 17:45:27 -0600

    reorganize

---
+++ web_fundamental/desserts/script.js
@@ -0,0 +1,28 @@
+function remove(element) {
+    element.remove()
+}
+
+function tiramisu() {
+    alert('You are searching for "tiramisu"')
+}
+
+var clicks = 0;
+function likes() {
+    clicks++
+    document.getElementById("clicks").innerHTML = clicks
+}
+
+
+var clicks1 = 0;
+function likes1() {
+    clicks1++
+    document.getElementById("clicks1").innerHTML = clicks1
+}
+
+
+
+var clicks2 = 0;
+function likes2() {
+    clicks2++
+    document.getElementById("clicks2").innerHTML = clicks2
+}
